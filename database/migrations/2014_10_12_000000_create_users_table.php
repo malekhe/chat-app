3b2a6cc17d4cac1ca9c4f992c8e72cd809d251e4
Hellal Malek <malekhellal13@gmail.com>
2022-06-03 15:41:55 +0200

    Third commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
           
           
-            $table->id('user_id');
+            $table->id('id');
            
             $table->string('fname',);
             $table->string('lname');
